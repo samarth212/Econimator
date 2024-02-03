162b55c5f188f722c9901b56331c544d0e8cc938
Samarth <samarth212@gmail.com>
2024-02-03 18:47:40 -0500

    header

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,12 @@ function Home(){
     
     return(
         <>
+        <div className="header">
+            <p>About</p>
+            <p>Home</p>
+            <p>Create</p>
+
+        </div>
         <p className="home-title">Econimator</p>
         
         </>
