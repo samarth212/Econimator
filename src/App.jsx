ecf0b46391820d40536b7295c123e9671cdea8f6
Samarth <samarth212@gmail.com>
2024-02-04 10:55:03 -0500

    changed react home

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import './App.css'
 import Header from './Header.jsx'
 
-
 import Home from './Home.jsx'
+import About from './About.jsx'
+import Create from './Create.jsx'
 
 function App() {
   let Component
   switch (window.location.pathname){
-    case: "/":
-    Component = Home
-    case: "/about":
-    Component = 
-    case: "/create":
-    Component
+    case "/":
+      Component = <Home></Home>
+      break
+    case "/about":
+      Component = <About></About>
+      break
+    case "/create":
+      Component = <Create></Create>
+      break
   }
-
-
-
   return (
     <>
       <Header></Header>
+      {Component}
     </>
   )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,31 +1,26 @@
 import React from 'react';
 import redPinkGradient from './assets/red-pink-gradient.png';
-
-
 import './home.css'
+
+
 function Home(){
     
     return(
         <>
-        <div className="header">
-            <p>About</p>
-            <p>Home</p>
-            <p>Create</p>
-        </div>
         <p className="home-title">Econimator</p>
         <p className="home-subtitle">Design your own market graphs.</p>
         <div className="home-buttons-container">
 
             <div className="home-button">
-                <img src={redPinkGradient} alt="" />
+                <img className="home-img" src={redPinkGradient} alt="" />
                 <p>Basic Demand and Supply</p>
             </div>
             <div className="home-button">
-                <img src={redPinkGradient} alt="" />
+                <img className="home-img" src={redPinkGradient} alt="" />
                 <p>Market Failure</p>
             </div>
             <div className="home-button">
-                <img src={redPinkGradient} alt="" />
+                <img className="home-img" src={redPinkGradient} alt="" />
                 <p>Custom</p>
             </div>
 
