b1a8055c8693d3e9ace312304b4d7a13e1ff3f8d
Samarth <samarth212@gmail.com>
2024-02-04 18:54:40 -0500

    added x axis functions

--- src/x-axis.jsx
+++ src/x-axis.jsx
@@ -3,8 +3,22 @@ import { useState } from 'react'
 export default function XAxis(){
     const [xAxisProps, setXAxisProps] = useState({
         numTicks: 0,
-        numInc: 0
+        numInc: 0,
     })
 
-    
+    function handleInput(e){
+        setXAxisProps({
+          ...xAxisProps,
+          [e.target.name]: parseInt(e.target.value, 10),
+        })
+    }
+
+    function renderXTicks(){
+        const ticks = []
+        for (let i = 0; i < xAxisProps.numTicks; i++) {
+            const tickValue = i * xAxisProps.tickIncrement
+            ticks.push(tickValue)
+        }
+        return ticks
+    }
 }
