c068276195f62d195c00a1811e6eab0c842dae2d
Samarth <samarth212@gmail.com>
2024-02-03 20:46:54 -0500

    tried to fix structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css'
-import './Home.jsx'
 import Header from './Header.jsx'
 
+
 import Home from './Home.jsx'
-import About from './About.jsx'
 
 function App() {
 
   return (
     <>
       <Header></Header>
-      <Home></Home>
     </>
   )
 }
