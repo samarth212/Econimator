386c90365845d8ca2b09cd987c50021450ffaaac
Samarth <samarth212@gmail.com>
2024-02-10 20:50:02 -0500

    created base component for inputs

---
+++ src/Inputs.jsx
@@ -0,0 +1,12 @@
+export default function Input(){
+    return(
+        <>
+            <input type="text"/>
+            <input type="text"/>
+            <input type="text"/>
+            <input type="number"/>
+
+        
+        </>
+    )
+}
