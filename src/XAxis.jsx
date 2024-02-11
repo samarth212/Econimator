ed39e3ddfc37ee740093d076bf8cfa7d6fdaf9f1
Samarth <samarth212@gmail.com>
2024-02-10 19:32:07 -0500

    added movable functionality for tick

--- src/XAxis.jsx
+++ src/XAxis.jsx
@@ -1,10 +1,18 @@
 import './xaxis.css'
+import { useState } from 'react'
+
 
 export default function XAxis(){
+
+    const distance = 50
+
     return(
         <>
-            <div className="xaxis-line"></div>
-            <div className="x-tick"></div>
+            <div className="xaxis-line">
+                <div className="x-tick" style={{ left: `${distance}px` }}></div>
+
+
+            </div>
         </>
     )
 }
