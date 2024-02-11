3a90720ed7193d9913c1a03cc334d72445b7cb32
Samarth <samarth212@gmail.com>
2024-02-10 19:44:11 -0500

    added baseline vars for ticks

--- src/XAxis.jsx
+++ src/XAxis.jsx
@@ -4,12 +4,19 @@ import { useState } from 'react'
 
 export default function XAxis(){
 
-    const distance = 50
+    const numTicks = 5
+    const spacing = 800/(numTicks-1)
+
+    let tickPositions = [];
+
+    for (let i = 0; i < numTicks; i++) {
+        tickPositions.push(i * spacing);
+    }
 
     return(
         <>
             <div className="xaxis-line">
-                <div className="x-tick" style={{ left: `${distance}px` }}></div>
+                <div className="x-tick"></div>
 
 
             </div>
