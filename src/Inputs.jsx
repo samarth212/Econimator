64ef326c58c4e9f4a6cc633c98c9d258b686eb30
Samarth <samarth212@gmail.com>
2024-02-11 12:18:01 -0500

    added styling to inputs

--- src/Inputs.jsx
+++ src/Inputs.jsx
@@ -3,11 +3,16 @@ import './inputs.css'
 export default function Input(){
     return(
         <>
-            <div className="title-inputs">
-                <input type="text" placeholder="Graph Title"/>
-                <input type="text" placeholder="X-Axis Title"/>
-                <input type="text" placeholder="Y-Axis Title"/>
+            <div className="inputs">
+                <div className="title-inputs">
+                    <input type="text" placeholder="Graph Title"/>
+                    <input type="text" placeholder="X-Axis Title"/>
+                    <input type="text" placeholder="Y-Axis Title"/>
+                </div>
+
+                
             </div>
+            
             
 
         
